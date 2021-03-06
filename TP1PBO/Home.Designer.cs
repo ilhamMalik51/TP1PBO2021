b41b62ed164aede67bd2d49a8ba9df0837dcff02
Muhammad Ilham Malik <79507977+ilhamMalik51@users.noreply.github.com>
2021-03-06 21:42:09 +0700

    Add files via upload

---
+++ TP1PBO/Home.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace TP1PBO
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNama = new System.Windows.Forms.Label();
+            this.NIM = new System.Windows.Forms.Label();
+            this.cboJenisBarang = new System.Windows.Forms.ComboBox();
+            this.cboHarga = new System.Windows.Forms.ComboBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.lblJenisBarang = new System.Windows.Forms.Label();
+            this.lblRange = new System.Windows.Forms.Label();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.lblJudulBarang = new System.Windows.Forms.Label();
+            this.lblKatalog = new System.Windows.Forms.LinkLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnKembali = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(932, 21);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(176, 20);
+            this.lblNama.TabIndex = 0;
+            this.lblNama.Text = "Muhammad Ilham Malik";
+            // 
+            // NIM
+            // 
+            this.NIM.AutoSize = true;
+            this.NIM.Location = new System.Drawing.Point(932, 58);
+            this.NIM.Name = "NIM";
+            this.NIM.Size = new System.Drawing.Size(72, 20);
+            this.NIM.TabIndex = 2;
+            this.NIM.Text = "1902563";
+            // 
+            // cboJenisBarang
+            // 
+            this.cboJenisBarang.FormattingEnabled = true;
+            this.cboJenisBarang.Items.AddRange(new object[] {
+            "Elektronik",
+            "Baju",
+            "Makanan"});
+            this.cboJenisBarang.Location = new System.Drawing.Point(10, 98);
+            this.cboJenisBarang.Name = "cboJenisBarang";
+            this.cboJenisBarang.Size = new System.Drawing.Size(180, 28);
+            this.cboJenisBarang.TabIndex = 3;
+            this.cboJenisBarang.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cboHarga
+            // 
+            this.cboHarga.FormattingEnabled = true;
+            this.cboHarga.Items.AddRange(new object[] {
+            "100.000 - 200.000",
+            "200.000 - 500.000",
+            "500.000 - 1.000.000"});
+            this.cboHarga.Location = new System.Drawing.Point(10, 174);
+            this.cboHarga.Name = "cboHarga";
+            this.cboHarga.Size = new System.Drawing.Size(180, 28);
+            this.cboHarga.TabIndex = 4;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(10, 237);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(180, 108);
+            this.btnFilter.TabIndex = 5;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // lblJenisBarang
+            // 
+            this.lblJenisBarang.AutoSize = true;
+            this.lblJenisBarang.Location = new System.Drawing.Point(10, 72);
+            this.lblJenisBarang.Name = "lblJenisBarang";
+            this.lblJenisBarang.Size = new System.Drawing.Size(102, 20);
+            this.lblJenisBarang.TabIndex = 6;
+            this.lblJenisBarang.Text = "Jenis Barang";
+            // 
+            // lblRange
+            // 
+            this.lblRange.AutoSize = true;
+            this.lblRange.Location = new System.Drawing.Point(10, 142);
+            this.lblRange.Name = "lblRange";
+            this.lblRange.Size = new System.Drawing.Size(105, 20);
+            this.lblRange.TabIndex = 7;
+            this.lblRange.Text = "Range Harga";
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblHome.Location = new System.Drawing.Point(350, 30);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(52, 20);
+            this.lblHome.TabIndex = 8;
+            this.lblHome.Text = "Home";
+            this.lblHome.Click += new System.EventHandler(this.lblHome_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(690, 21);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(105, 48);
+            this.btnLogout.TabIndex = 10;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // lblJudulBarang
+            // 
+            this.lblJudulBarang.AutoSize = true;
+            this.lblJudulBarang.Location = new System.Drawing.Point(226, 79);
+            this.lblJudulBarang.Name = "lblJudulBarang";
+            this.lblJudulBarang.Size = new System.Drawing.Size(124, 20);
+            this.lblJudulBarang.TabIndex = 11;
+            this.lblJudulBarang.Text = "Semua Barang :";
+            this.lblJudulBarang.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblKatalog
+            // 
+            this.lblKatalog.AutoSize = true;
+            this.lblKatalog.Location = new System.Drawing.Point(515, 30);
+            this.lblKatalog.Name = "lblKatalog";
+            this.lblKatalog.Size = new System.Drawing.Size(96, 20);
+            this.lblKatalog.TabIndex = 12;
+            this.lblKatalog.TabStop = true;
+            this.lblKatalog.Text = "WebKatalog";
+            this.lblKatalog.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblKatalog_LinkClicked);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
+            this.panel1.Controls.Add(this.btnKembali);
+            this.panel1.Controls.Add(this.lblRange);
+            this.panel1.Controls.Add(this.lblJenisBarang);
+            this.panel1.Controls.Add(this.btnFilter);
+            this.panel1.Controls.Add(this.cboHarga);
+            this.panel1.Controls.Add(this.cboJenisBarang);
+            this.panel1.Location = new System.Drawing.Point(2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(197, 523);
+            this.panel1.TabIndex = 13;
+            // 
+            // btnKembali
+            // 
+            this.btnKembali.Location = new System.Drawing.Point(10, 382);
+            this.btnKembali.Name = "btnKembali";
+            this.btnKembali.Size = new System.Drawing.Size(114, 48);
+            this.btnKembali.TabIndex = 14;
+            this.btnKembali.Text = "Kembali";
+            this.btnKembali.UseVisualStyleBackColor = true;
+            this.btnKembali.Visible = false;
+            this.btnKembali.Click += new System.EventHandler(this.btnKembali_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1385, 520);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblKatalog);
+            this.Controls.Add(this.lblJudulBarang);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.lblHome);
+            this.Controls.Add(this.NIM);
+            this.Controls.Add(this.lblNama);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.Label NIM;
+        private System.Windows.Forms.ComboBox cboJenisBarang;
+        private System.Windows.Forms.ComboBox cboHarga;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Label lblJenisBarang;
+        private System.Windows.Forms.Label lblRange;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label lblJudulBarang;
+        private System.Windows.Forms.LinkLabel lblKatalog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnKembali;
+    }
+}
---
+++ TP1PBO/Home.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1PBO
+{
+    public partial class Home : Form
+    {
+        List<Barang> listBarang = new List<Barang>();
+
+        public Home()
+        {
+            InitializeComponent();
+            Barang barang1 = new Barang("Jersey", "Baju", 120000, "120.000");
+            Barang barang2 = new Barang("Gelato", "Makanan", 145000, "145.000");
+            Barang barang3 = new Barang("Laptop", "Elektronik", 1000000, "1.000.000");
+            Barang barang4 = new Barang("Mouse", "Elektronik", 250000, "250.000");
+            Barang barang5 = new Barang("Gucci", "Baju", 750000, "750.000");
+
+            barang1.alamatFoto = @"C:\Users\USER\source\repos\TP1PBO\TP1PBO\images\jersey.jpg";
+            barang2.alamatFoto = @"C:\Users\USER\source\repos\TP1PBO\TP1PBO\images\gelato.jpg";
+            barang3.alamatFoto = @"C:\Users\USER\source\repos\TP1PBO\TP1PBO\images\laptop.png";
+            barang4.alamatFoto = @"C:\Users\USER\source\repos\TP1PBO\TP1PBO\images\mouse.jpg";
+            barang5.alamatFoto = @"C:\Users\USER\source\repos\TP1PBO\TP1PBO\images\gucci.jpg";
+            
+            listBarang.Add(barang1);
+            listBarang.Add(barang2);
+            listBarang.Add(barang3);
+            listBarang.Add(barang4);
+            listBarang.Add(barang5);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            direction("baru", null);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void tmplBarang(int left, Barang barang)
+        {
+            Label hargaBarang = new Label();
+            this.Controls.Add(hargaBarang);
+
+            Label namaBarang = new Label();
+            this.Controls.Add(namaBarang);
+
+            PictureBox ktkFoto = new PictureBox();
+            this.Controls.Add(ktkFoto);
+
+            Button btnBeli = new Button();
+            this.Controls.Add(btnBeli);
+
+            // 
+            // hargaBarang
+            // 
+            hargaBarang.AutoSize = true;
+            hargaBarang.Location = new System.Drawing.Point(left + 51, 244);
+            hargaBarang.Name = "hargaBarang";
+            hargaBarang.Size = new System.Drawing.Size(58, 20);
+            hargaBarang.TabIndex = 14;
+            hargaBarang.Text = "Rp." + barang.tmplHarga;
+            // 
+            // namaBarang
+            // 
+            namaBarang.AutoSize = true;
+            namaBarang.Location = new System.Drawing.Point(left + 55, 224);
+            namaBarang.Name = "namaBarang";
+            namaBarang.Size = new System.Drawing.Size(111, 20);
+            namaBarang.TabIndex = 13;
+            namaBarang.Text = barang.namaBarang;
+            // 
+            // btnBeli
+            // 
+            btnBeli.Location = new System.Drawing.Point(left + 29, 267);
+            btnBeli.Name = "btnBeli";
+            btnBeli.Size = new System.Drawing.Size(99, 37);
+            btnBeli.TabIndex = 15;
+            btnBeli.Text = "Beli";
+            btnBeli.UseVisualStyleBackColor = true;
+            btnBeli.Click += new System.EventHandler((s , e) => btnBeli_Click(s , e, barang.namaBarang));
+            // 
+            // ktkFoto
+            // 
+            ktkFoto.Location = new System.Drawing.Point(left, 76);
+            ktkFoto.Name = "ktkFoto";
+            ktkFoto.Size = new System.Drawing.Size(120, 146);
+            ktkFoto.TabIndex = 12;
+            ktkFoto.TabStop = false;
+            ktkFoto.Image = Image.FromFile(barang.alamatFoto);
+
+        }
+
+        private void direction(string status, string btnBeli)
+        {
+            int left = 120;
+
+            if (status == "baru")
+            {
+                foreach (Barang bykBarang in listBarang)
+                {
+                    tmplBarang(left, bykBarang);
+                    left += 150;
+                }
+            }
+            else if (status == "filter")
+            {
+                int batasBawah = 0;
+                int batasAtas = 0;
+
+                if(Convert.ToString(cboHarga.SelectedItem) == "100.000 - 200.000")
+                {
+                    batasBawah = 100000;
+                    batasAtas = 200000;
+                }
+                else if (Convert.ToString(cboHarga.SelectedItem) == "200.000 - 500.000")
+                {
+                    batasBawah = 200000;
+                    batasAtas = 500000;
+                }
+                else if(Convert.ToString(cboHarga.SelectedItem) == "500.000 - 1.000.000")
+                {
+                    batasBawah = 500000;
+                    batasAtas = 1000000;
+                }
+
+
+                foreach (Barang bykBarang in listBarang)
+                {
+                    if(bykBarang.jenisBarang == Convert.ToString(cboJenisBarang.SelectedItem))
+                    {
+                        if(bykBarang.hargaBarang >= batasBawah && bykBarang.hargaBarang <= batasAtas)
+                        {
+                            tmplBarang(left, bykBarang);
+                            left += 150;
+                        }
+                    }
+                }
+            }
+            else if (status == "beli" && btnBeli != null)
+            {
+                foreach (Barang bykBarang in listBarang)
+                {
+                    if(bykBarang.namaBarang == btnBeli)
+                    {
+                        tmplBarang(left, bykBarang);
+                    }
+                }
+            }
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            if(cboHarga.SelectedItem == null && cboJenisBarang.SelectedItem == null)
+            {
+                MessageBox.Show("Tolong Masukan Kedua Filter");
+            }
+            else
+            {
+                empty();
+                direction("filter", null);
+                btnKembali.Visible = false;
+            }
+        }
+
+        private void empty()
+        {
+            
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name == "hargaBarang" || c.Name == "namaBarang" || c.Name == "ktkFoto" || c.Name == "btnBeli")
+                {
+                    c.Visible = false;
+                }
+            }
+        }
+
+        private void lblKatalog_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://shopee.co.id/");
+        }
+
+        private void lblHome_Click(object sender, EventArgs e)
+        {
+            empty();
+            direction("baru", null);
+            btnKembali.Visible = false;
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            LoginPage f1 = new LoginPage();
+            f1.Show();
+
+            this.Hide();
+            this.Close();
+        }
+
+        private void btnBeli_Click(object sender, EventArgs e, string nama)
+        {
+            empty();
+            direction("beli", nama);
+            btnKembali.Visible = true;
+            lblJudulBarang.Text = "Detail Barang:";
+        }
+
+        private void btnKembali_Click(object sender, EventArgs e)
+        {
+            empty();
+            direction("baru", null);
+            btnKembali.Visible = false;
+            lblJudulBarang.Text = "Semua Barang:";
+        }
+    }
+}
